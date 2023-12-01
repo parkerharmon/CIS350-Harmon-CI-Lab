729199527ee2d6933ce7a9380fe845b0860b65e2
Parker <parkerharmon194@gmail.com>
2023-12-01 13:36:54 -0500

    initial commit

---
+++ math_functions.py
@@ -0,0 +1,14 @@
+def add_numbers(a, b):
+	return a + b
+def subtract_numbers(a, b):
+	return a - b	
+def multiply_numbers(a,b):
+	return a * b
+def divide_numbers(a,b):
+	return a / b
+	
+if __name__ == "__main__":
+	print("Adding:", add_numbers(2,4))
+	print("Subtracting:", subtract_numbers(9,2))
+	print("Multiplying:", multiply_numbers(3,3))
+	print("Dividing:", divide_numbers(3,3))
